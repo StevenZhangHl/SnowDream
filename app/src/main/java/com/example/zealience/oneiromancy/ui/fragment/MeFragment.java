6655536675aa3d47ec79f0685ce82ae6728da982
steven945 <15921327326@163.com>
2019-03-08 17:08:36 +0800

    1.增加我的页

--- app/src/main/java/com/example/zealience/oneiromancy/ui/fragment/MeFragment.java
+++ app/src/main/java/com/example/zealience/oneiromancy/ui/fragment/MeFragment.java
@@ -3,7 +3,16 @@ package com.example.zealience.oneiromancy.ui.fragment;
 import android.os.Bundle;
 
 import com.example.zealience.oneiromancy.R;
+import com.scwang.smartrefresh.header.BezierCircleHeader;
+import com.scwang.smartrefresh.header.DeliveryHeader;
+import com.scwang.smartrefresh.header.PhoenixHeader;
+import com.scwang.smartrefresh.header.TaurusHeader;
+import com.scwang.smartrefresh.header.WaterDropHeader;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.header.BezierRadarHeader;
 import com.steven.base.base.BaseFragment;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * @user steven
@@ -11,6 +20,9 @@ import com.steven.base.base.BaseFragment;
  * @description 我的页面
  */
 public class MeFragment extends BaseFragment {
+    private SmartRefreshLayout refreshMe;
+    private CircleImageView ivMeHead;
+
     public static MeFragment newInstance(String title) {
         Bundle bundle = new Bundle();
         bundle.putString("title", title);
@@ -31,7 +43,10 @@ public class MeFragment extends BaseFragment {
 
     @Override
     public void initView(Bundle savedInstanceState) {
-
+        refreshMe = (SmartRefreshLayout) rootView.findViewById(R.id.refresh__me);
+        ivMeHead = (CircleImageView) rootView.findViewById(R.id.iv_me_head);
+        refreshMe.setPrimaryColors(_mActivity.getResources().getColor(R.color.mainColor));
+        refreshMe.setRefreshHeader(new BezierRadarHeader(_mActivity));
     }
 
     @Override
