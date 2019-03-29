5558c5b9a3f48d58e988d98b810d04e234ac6ef5
steven945 <15921327326@163.com>
2019-03-29 17:18:11 +0800

    1.增加App弹屏广告

--- app/src/main/java/com/example/zealience/oneiromancy/constant/UrlConstant.java
+++ app/src/main/java/com/example/zealience/oneiromancy/constant/UrlConstant.java
@@ -7,6 +7,6 @@ package com.example.zealience.oneiromancy.constant;
  */
 public class UrlConstant {
     public static final String URL_MYGITHUB_HOME = "https://github.com/steven945";
-    public static final String url_app_adv_photo = "http://imgk.zol.com.cn/sjbbs/3469/a3468634.jpg";
+    public static final String url_app_adv_photo = "http://sjbz.fd.zol-img.com.cn/t_s480x800c/g5/M00/00/04/ChMkJ1fJWFiIH_xjAAf28G2nvpoAAU-JQH-Dx0AB_cI468.jpg";
     public static final String url_app_adv_function = "https://www.pgyer.com/i4H2";
 }
